865bec493ad7784bff92f40ac3d3283cde786448
OikNao <oiknine123@gmail.com>
2021-12-22 18:10:57 -0500

    removed "seconds"

--- index.js
+++ index.js
@@ -16,12 +16,9 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let seconds = date.getSeconds();
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
+
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}:${seconds}`;
+  return `${day} ${hours}:${minutes}`;
 }
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
